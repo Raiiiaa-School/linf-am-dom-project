e20e855d8b15cf0a5cc078f7d7a01592cfc9aa85
RVentura <240001108@esg.ipsantarem.pt>
2025-04-07 14:05:26 +0100

    Logica do Timer

--- euro2016.js
+++ euro2016.js
@@ -1,16 +1,30 @@
 import { Gameboard } from "./js/gameboard.js";
+import { Timer } from "./js/timer.js";
 
 /**
  * @type {Gameboard | undefined}
  */
 let gameboard;
+let timer;
 
 window.addEventListener("load", init, false);
+
+window.addEventListener("keydown", (e) => {
+  if (e.code === "Space") {
+      if (timer) {
+          timer.reset();
+          timer.start();
+      }
+  }
+});
 
 async function init() {
     gameboard = new Gameboard();
     const jsonData = await gameboard.loadJSON("./assets/oitavos.json");
     gameboard.createCards(jsonData);
+
+    timer = new Timer(gameboard);
+    timer.start();
 }
 
 /**
--- js/timer.js
+++ js/timer.js
@@ -1,3 +1,58 @@
 export class Timer {
-    // TODO: https://github.com/Raiiiaa-School/linf-am-dom-project/issues/9
+    constructor(gameboard) {
+        this._intervalId = null;
+        this._currentTime = 0;
+        this._maxTime = 45;
+        this._progressEl = document.getElementById("time");
+        this._gameboard = gameboard;
+        this._updateProgressBar();
+    }
+
+    start() {
+        this.reset();
+        this._intervalId = setInterval(() => {
+            this._currentTime++;
+            this._updateProgressBar();
+
+            if (this._currentTime === this._maxTime) {
+                clearInterval(this._intervalId);
+                this._showOverlay();
+
+                // Baralhar cartas após o tempo esgotar
+                if (this._gameboard) {
+                    this._gameboard.shuffleCards();
+                }
+            }
+        }, 1000);
+    }
+
+    reset() {
+        clearInterval(this._intervalId);
+        this._currentTime = 0;
+        this._updateProgressBar();
+    }
+
+    _updateProgressBar() {
+        if (this._progressEl) {
+            this._progressEl.value = this._currentTime;
+
+            if (this._currentTime >= 40) {
+                this._progressEl.classList.add("warning");
+            } else {
+                this._progressEl.classList.remove("warning");
+            }
+        }
+    }
+
+    _showOverlay() {
+        const overlay = document.getElementById("overlay");
+        if (overlay) {
+            overlay.classList.remove("hidden");
+    
+            // Esconde o overlay após 3 segundos
+            setTimeout(() => {
+                overlay.classList.add("hidden");
+            }, 3000);
+        }
+    }
 }
